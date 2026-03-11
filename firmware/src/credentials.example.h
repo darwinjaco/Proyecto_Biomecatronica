93ffc37cf9d262167644713e82b3f2c018f11814
darwinjaco <106838535+darwinjaco@users.noreply.github.com>
2026-03-11 13:29:07 -0500

    Create credentials.example.h

---
+++ firmware/src/credentials.example.h
@@ -0,0 +1,11 @@
+// ============================================================
+//  INSTRUCCIONES:
+//  1. Copia este archivo y renombralo como: credentials.h
+//  2. Rellena con tus datos reales
+//  3. NUNCA subas credentials.h a GitHub (ya esta en .gitignore)
+// ============================================================
+
+#pragma once
+
+#define WIFI_SSID      "TU_RED_WIFI"
+#define WIFI_PASSWORD  "TU_CONTRASENA_WIFI"
